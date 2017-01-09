42ba99be98465825a80f418744698c965b3abea0
rajankonar <rajankonar@live.in>
2017-01-10 05:09:39 +0530

    updated about section

--- assets/js/all.js
+++ assets/js/all.js
@@ -45,18 +45,22 @@ $(document).ready(function () {
     
 
 
-var controller = new ScrollMagic.Controller();
+var controller = new ScrollMagic.Controller(),
 // var $animateThis = $('#animateThis');
-var $firstname = $('.name-firstname');
-var $firstname_span = $('.name-firstname>span');
-var $surname = $(".surname-block");
-var $me_saying = $('.me-saying');
-var $say_noob = $(".say-noob");
+ $firstname = $('.name-firstname'),
+ $firstname_span = $('.name-firstname>span'),
+ $surname = $(".surname-block"),
+ $me_saying = $('.me-saying'),
+ $say_noob = $(".say-noob"),
+ $section_about = $("#about"),
+ $beer_img = $(".img-beer"),
+ $profile_picture = $(".img-profile-picture");
 
-var all_elements=[$firstname_span,$surname,$me_saying,$say_noob];
+var all_elements=[$firstname_span,$surname,$me_saying,$say_noob,$section_about];
 
 var tl = new TimelineMax();
-
+var tl_about = new TimelineMax();
+var tl_scroll = new TimelineMax();
 /*$firstname.html($firstname.html().replace(/./g, "<span>$&</span>").replace(/\s/g, " "));*/
 
 /* TweenLite.from($animateThis, 2, {x: '-=200px', autoAlpha: 0});
@@ -86,20 +90,55 @@ tl.fromTo($surname, 3, {autoAlpha: 0}, { autoAlpha:0.8}, 1)
         {x:$say_noob.x + 10, y:$say_noob.y - 20, scaleX: 1,   scaleY: 1},
         {x:$say_noob.x + 20, y:$say_noob.y + 20, scaleX: 1.2, scaleY: .8},
         {x:$say_noob.x + 20, y:$say_noob.y + 30, scaleX: 1,   scaleY: 1}],
-        orientToBezier: false,
+       
         ease:Power3.easeOut,yoyo:true, repeat:-1, repeatDelay:0, yoyo:true
-    });
-
+    })
+ .fromTo($section_about, 5, {autoAlpha: 0},{ autoAlpha: 1},10);
+   
 /* .to($say_noob, 0.2, {css:{scaleX:0.5}, ease:Power3.easeOut})
 .to($say_noob, 0.2, {css:{scaleX:1.5}, ease:Power3.easeOut})
 .to($say_noob, 0.2, {css:{scaleX:1}, ease:Power3.easeOut});
 */
-//var scene = new ScrollMagic.Scene({triggerElement: "#screen2"}).addIndicators({name: "1 (duration: 0)"})    .setTween(tl).addTo(controller);
+    
+   tl_about.to($profile_picture, 0.4, {top: '-135px', ease:Power0.easeOut})
+       .fromTo($beer_img, 0.4, {visible:true,scale:0},{scale:1,ease:Back.easeOut});
+    
+    
+   tl_scroll.to($section_about, 0.4, {top: '-135px', ease:SlowMo.easeOut});
+       
+    
+var scene_profile_picture = new ScrollMagic.Scene({triggerElement: "#about"}).addIndicators({name: "1 (duration: 0)"})    .setTween(tl_about).addTo(controller);
+var scene3 = new ScrollMagic.Scene({triggerElement: "#about"}).addIndicators({name: "1 (duration: 0)"})    .setTween(tl_scroll).addTo(controller);
 
+    
 window.onload = function () {
     tl.play();
 }
 
+
+/* Smooth scroll  */
+
+$(".navbar-brand a,.navbar-links a").on("click", function(e) {
+		var $this = $(this),
+			  $href = $this.attr("href"),
+			  $target = $("#" + $href),
+			  $navHeight = $(".navigation").outerHeight()+150,
+			  $targetTopY = $target.offset().top - $navHeight;
+		/* update active class for nav elements */
+		$(".navbar-links a").removeClass("active");
+		$this.addClass("active");
+		
+		/* smooth scroll */
+		TweenMax.to($(window), 1, {
+			scrollTo: {
+				y: $targetTopY,
+				autoKill: true
+			},
+			ease: "Expo.easeInOut"
+		});
+		
+		e.preventDefault();
+	});
 
  });
 
